6efc58f2f80e8db16496232b8862e05e5d9fdbf0
gi rocha <133895231+gymratgi0@users.noreply.github.com>
2023-12-13 08:14:17 -0300

    Delete prjFinal23-main directory

--- prjFinal23-main/src/main/java/com/prjFinal/PowerDevs/repositories/JogoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prjFinal.PowerDevs.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prjFinal.PowerDevs.entities.Jogo;
-
-public interface JogoRepository extends JpaRepository<Jogo, Long> {
-
-}
